cdd3b71cb94730817b09322e2583bc07177a9665
Sarah Ayanwale <sarahayanwale@gmail.com>
2022-05-02 10:11:59 +0200

    starter template created

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/icons.css"
+
+const App = () => {
+  return <div className="linkedIn-body"></div>
+}
+
+export default App
