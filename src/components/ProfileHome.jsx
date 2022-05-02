ab86ed41a19a3619b2392181e4e62b85b66b4322
Sarah Ayanwale <sarahayanwale@gmail.com>
2022-05-02 10:38:56 +0200

    profile added

---
+++ src/components/ProfileHome.jsx
@@ -0,0 +1,13 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+const ProfileHome = () => {
+  return (
+    <Container>
+      <Row>
+        <Col md={8}></Col>
+        <Col md={4}></Col>
+      </Row>
+    </Container>
+  )
+}
+export default ProfileHome
