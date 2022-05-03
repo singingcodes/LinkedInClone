d05437edbb01f1d0e887221b373728d16703e65d
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-05-03 09:17:33 +0200

    modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-icons/font/bootstrap-icons.css"
-import ProfileHome from "./components/ProfileHome"
-import MyNavBar from "./components/MyNavBar"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import MyFooter from "./components/MyFooter"
-import NotFound from "./components/NotFound"
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import ProfileHome from "./components/ProfileHome";
+import MyNavBar from "./components/MyNavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyFooter from "./components/MyFooter";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
@@ -19,7 +19,7 @@ const App = () => {
         <MyFooter />
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
