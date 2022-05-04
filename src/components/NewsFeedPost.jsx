11843db952ec579237acf224871db5c9b4691389
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-05-04 19:23:42 +0100

    Post feed section

--- src/components/NewsFeedPost.jsx
+++ src/components/NewsFeedPost.jsx
@@ -1,5 +1,104 @@
-const NewsFeedPost = () => {
-  return <h5>Post Feed</h5>
-}
+import styled from "styled-components";
+import { FcStackOfPhotos, FcVideoCall } from "react-icons/fc";
+import PostSection from "./PostSection";
 
-export default NewsFeedPost
+const NewsFeedPost = () => {
+  return (
+    <>
+      <Wrapper>
+        <Header>
+          <img src="https://placekitten.com/100/100" alt="profile-avatar" />
+          <Input type="text" placeholder="Start post" />
+        </Header>
+        <Footer>
+          <Section>
+            <FcStackOfPhotos size="1.5rem" />
+            <span className="ml-2">Photo</span>
+          </Section>
+          <Section>
+            <FcVideoCall size="1.5rem" />
+            <span className="ml-2">Video</span>
+          </Section>
+          <Section>
+            <img
+              src="https://img.icons8.com/color/48/000000/clock--v4.png"
+              alt="event"
+            />
+            Event
+          </Section>
+          <Section>
+            <img
+              src="https://img.icons8.com/color/48/000000/scroll.png"
+              alt="article"
+            />
+            Write article
+          </Section>
+        </Footer>
+      </Wrapper>
+
+      <hr />
+
+      <PostSection />
+    </>
+  );
+};
+
+export default NewsFeedPost;
+
+const Wrapper = styled.div`
+  padding: 1rem 1rem;
+  min-height: 5rem;
+  border-radius: 6px;
+  border: 1px solid #e6e6e6;
+  background-color: white;
+  margin-bottom: 1rem;
+`;
+
+const Header = styled.div`
+  height: 3rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  img {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    margin-right: 1rem;
+  }
+`;
+
+const Input = styled.input`
+  flex-grow: 1;
+  height: 100%;
+  border-radius: 25px;
+  border: 1px solid #adadad;
+  padding: 0 1rem;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 3rem;
+  width: 100%;
+  margin-top: 1rem;
+`;
+
+const Section = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  transition: 0.3s;
+  padding: 0.7rem 0.5rem;
+  border-radius: 4px;
+
+  img {
+    width: 1.5rem;
+    margin-right: 0.5rem;
+  }
+
+  &:hover {
+    background-color: #dddddd;
+  }
+`;
